0fbf522c2df253d07e4d97e834d3cc065b7dc407
NMTuan <47388278+nmtuan1612@users.noreply.github.com>
2023-03-06 15:20:12 +0700

    config cors

--- index.js
+++ index.js
@@ -21,9 +21,14 @@ app.use("/images", express.static("images"))
 // Middleware
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors({
-  origin: 'https://nmtmedia.netlify.app'
-}));
+// app.use(cors({
+//   origin: 'https://nmtmedia.netlify.app'
+// }));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://nmtmedia.netlify.app"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 dotenv.config();
 
