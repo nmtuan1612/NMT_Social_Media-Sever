da72043eef0e382a97aa24f8e41b6a6d486836b4
NMTuan <tuan.nguyen@sunbuy.vn>
2023-06-08 02:07:38 +0700

    remove package

--- MiddleWare/uploadMiddleWare.js
+++ MiddleWare/uploadMiddleWare.js
@@ -1,26 +1,26 @@
-import { promisify } from "util";
-import multer from "multer";
-import { GridFsStorage } from "multer-gridfs-storage";
+// import { promisify } from "util";
+// import multer from "multer";
+// import { GridFsStorage } from "multer-gridfs-storage";
 
-const storage = new GridFsStorage({
-  url: process.env.MONGO_DB,
-  options: { useNewUrlParser: true, useUnifiedTopology: true },
-  file: (req, file) => {
-    const match = ["image/png", "image/jpeg"];
+// const storage = new GridFsStorage({
+//   url: process.env.MONGO_DB,
+//   options: { useNewUrlParser: true, useUnifiedTopology: true },
+//   file: (req, file) => {
+//     const match = ["image/png", "image/jpeg"];
 
-    if (match.indexOf(file.mimetype) === -1) {
-      const filename = `${Date.now()}${file.originalname}`;
-      return filename;
-    }
+//     if (match.indexOf(file.mimetype) === -1) {
+//       const filename = `${Date.now()}${file.originalname}`;
+//       return filename;
+//     }
 
-    return {
-      bucketName: process.env.MEDIA_BUCKET_NAME,
-      filename: `${Date.now()}${file.originalname}`,
-    };
-  },
-});
+//     return {
+//       bucketName: process.env.MEDIA_BUCKET_NAME,
+//       filename: `${Date.now()}${file.originalname}`,
+//     };
+//   },
+// });
 
-const uploadFiles = multer({ storage: storage }).single("file");
-const uploadFilesMiddleware = promisify(uploadFiles);
+// const uploadFiles = multer({ storage: storage }).single("file");
+// const uploadFilesMiddleware = promisify(uploadFiles);
 
-export default uploadFilesMiddleware;
+// export default uploadFilesMiddleware;
